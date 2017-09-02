f81198e821cbefb864d06fc47a05814fa25f2266
Sam Wemyss <samuel.wemyss@gmail.com>
2017-09-02 15:33:30 +1000

    :pencil: improve Sprite.js documentation

--- public/Sprite.js
+++ public/Sprite.js
@@ -1,16 +1,29 @@
 'use strict';
 
 class Sprite {
+  /**
+   * Constructor
+   * @param {Integer} [width] - number of horizontal pixels that make up a sprite segment/tile
+   * @param {Integer} [height] - number of vertical pixels that make up a sprite segment/tile
+   * @param {Image} [image] - image object of the spritesheet
+   * @param {Number} [x] - x position to draw sprite on the canvas
+   * @param {Number} [y] - y position to draw sprite on the canvas
+   */
   constructor(width, height, image, x, y) {
     Object.assign(this, {
       width,
       height,
       image,
-      x, // x position on canvas
-      y, // y position on canvas
+      x,
+      y,
     });
   }
 
+  /**
+   * Draw the sprite on the main canvas
+   * @param {Integer} [sx] - source x coordinate of the sprite segment to draw
+   * @param {Integer} [sy] - source y coordinate of the sprite segment to draw
+   */
   render(sx = 0, sy = 0) {
     $context.clearRect(0, 0, this.width, this.height);
     $context.drawImage(
