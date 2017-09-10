d1770db25c836f89e7be2c1dc532b64daa076305
Sam Wemyss <samuel.wemyss@gmail.com>
2017-09-10 23:01:27 +1000

    simple as fuck camera

---
+++ public/client/Camera.js
@@ -0,0 +1,25 @@
+
+class Camera {
+  constructor(x = 0, y = 0) {
+    this.x = x; // x center of camera
+    this.y = y; // y center of camera
+  }
+
+  clampCoordinate(coordinate) {
+    return (coordinate - (CAMERA_SIZE/2)).clamp(0, GRID_SIZE - CAMERA_SIZE);
+  }
+
+  update() {
+    this.x = this.clampCoordinate($player.x);
+    this.y = this.clampCoordinate($player.y);
+
+    $context.setTransform(
+      1,
+      0,
+      0,
+      1,
+      -this.x * SQUARE_PIXEL_SIZE,
+      -this.y * SQUARE_PIXEL_SIZE
+    );
+  }
+}
